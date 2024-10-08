a6ed1538646f93ab067f702afa8c619425653989
LuchoProgramer <luchoviteri1990@gmail.com>
2024-10-07 22:52:58 -0500

    allowed Host

--- inventario/settings.py
+++ inventario/settings.py
@@ -31,7 +31,7 @@ if ENVIRONMENT == 'production':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['167.71.241.147']
+ALLOWED_HOSTS = ['167.71.241.147', 'localhost', '127.0.0.1']
 
 
 # Application definition
